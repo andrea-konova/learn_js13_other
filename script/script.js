991f77466f8cf084e257ad91b0f4e7638c105150
Andrii Konovalenko <an256.ak@gmail.com>
2020-06-12 01:28:53 +0300

    feat: added missing functionality

--- script/script.js
+++ script/script.js
@@ -5,45 +5,66 @@ const isNumber = function(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
-let firstMessage;
+const startGame = function() {
+
+  let firstMessage;
     
-firstMessage = confirm('Угадай число от 1 до 100');
-console.log(firstMessage);
+  firstMessage = confirm('Угадай число от 1 до 100');
+  console.log(firstMessage);
 
-if (firstMessage == true) {
-  // функция угадай число
-  const guessNumber = function() {
-    const botNumber = 88;
-    let userNumber;
+  if (firstMessage == true) {
+    // функция угадай число
+    const guessNumber = function() {
+      const botNumber = 88;
+      let userNumber;
 
-    // функция получить число от пользователя
-    const getNumber = function() {
-      userNumber = prompt('Введи число от 1 до 100');
-      console.log(userNumber);
+      // функция завершить игру
+      const stopGame = function() {
+        alert('Игра завершена');
+      };
 
-      if (botNumber < userNumber) {
-        alert('Загаданное число меньше');
-        return getNumber();
-      }
-      if (botNumber > userNumber) {
-        alert('Загаданное число больше');
-        return getNumber();
-      }
-      if (!isNumber(userNumber)) {
-        alert('Введи число!');
-        return getNumber();
-      }
-      if (botNumber == userNumber) {
-        alert('Поздравляю! Ты угадал число!');
-      }
+      // функция получить число от пользователя
+      const getNumber = function() {
+        userNumber = prompt('Введи число от 1 до 100');
+        console.log(userNumber);
+
+        if (botNumber < userNumber) {
+          alert('Загаданное число меньше');
+          return getNumber();
+        } else if (userNumber == null) {
+          return stopGame();
+        }
+        if (botNumber > userNumber) {
+          alert('Загаданное число больше');
+          return getNumber();
+        } else if (userNumber == null) {
+          stopGame();
+        }
+        if (!isNumber(userNumber)) {
+          alert('Введи число!');
+          return getNumber();
+        } else if (userNumber == null) {
+          stopGame();
+        }
+        if (botNumber == userNumber) {
+          alert('Поздравляю! Ты угадал число!');
+          return startGame();
+        }
+      };
+      
+      return getNumber();
+
     };
-    
-    return getNumber();
+    guessNumber();
 
-  };
-  guessNumber();
+  } else {
+    // функция завершить игру
+    const stopGame = function() {
+      alert('Игра завершена');
+    };
+    stopGame();
+  }
+};
 
-} else {
-  alert('Игра завершена');
-}
+startGame();
 
