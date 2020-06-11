0b0315cc29db9f264a042d52cac2d7271a65f2ef
Andrii Konovalenko <an256.ak@gmail.com>
2020-06-11 17:39:32 +0300

    feat: created the functionality of the program

--- script/script.js
+++ script/script.js
@@ -1,1 +1,54 @@
 'use strict';
+
+// проверяет значение на число
+const isNumber = function(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+let firstMessage;
+    
+firstMessage = confirm('Угадай число от 1 до 100');
+console.log(firstMessage);
+
+if (firstMessage == true) {
+
+
+  // функция угадай число
+  const guessNumber = function() {
+    const botNumber = 88;
+    let userNumber;
+
+    // функция получить число от пользователя
+    const getNumber = function() {
+      userNumber = prompt('Введи число от 1 до 100');
+      console.log(userNumber);
+
+      if (botNumber < userNumber) {
+        alert('Загаданное число меньше');
+        return getNumber();
+      }
+  
+      if (botNumber > userNumber) {
+        alert('Загаданное число больше');
+        return getNumber();
+      }
+  
+      if (!isNumber(userNumber)) {
+        alert('Введи число!');
+        return getNumber();
+      }
+
+      if (botNumber == userNumber) {
+        alert('Поздравляю! Ты угадал число!');
+      }
+      
+    };
+    return getNumber();
+
+  };
+  guessNumber();
+
+} else {
+  alert('Игра завершена');
+}
+
